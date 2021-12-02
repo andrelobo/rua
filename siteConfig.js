2684d218d9b0c3f219580770d97d2d265aff0173
andrelobo <loboandre@hotmail.com>
2021-12-02 15:56:27 -0400

    Initial commits

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "R.U.A",
+  shortName: "R.U.A",
+  description: "Respeito, União e Atitude",
+  url: "https://andrelobo.github.io", // Domain of your site without prefix!
+  prefix: "/",
+  author: "André Lobo",
+  twitter: "@andrelobo73",
+}
